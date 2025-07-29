70c3af4b62990fd6f72998bedbc9c0252045cf33
Alessandro Masini <alessandro.masini@alten.it>
2025-07-29 13:19:01 +0200

    Add ioctl handling to aesdsocket

--- server/src/connection.c
+++ server/src/connection.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <netdb.h>
 #include <pthread.h>
 #include <stdbool.h>
@@ -9,6 +10,7 @@
 #include <sys/types.h>
 #include <syslog.h>
 #include <unistd.h>
+#include "../../aesd-char-driver/aesd_ioctl.h"
 
 #define CONN_BUFSIZE 256
 
@@ -64,55 +66,86 @@ void* conn_handler(void* handler_arg) {
     if (fd < 0) {
         syslog(LOG_ERR, "open: %s", strerror(errno));
         abort = true;
-        goto cleanup;
+        goto finalize;
     }
 
     // Receive packet from client. A packet ends when a newline is found in
     // the character stream obtained from the socket.
-    // If the packet is received correctly write its contend to file, then
-    // free memory. Otherwise stop execution.
-    // After packet is received, write to end of tmpfile.
+    // If the packet is received correctly write its content to file/do ioctl,
+    // then free memory. Otherwise stop execution.
     size_t packet_size;
     char* packet = sock_getline(connection->descriptor, &packet_size);
     if (!packet) {
         abort = true;
-        goto cleanup;
+        goto cleanup_fd;
     }
     syslog(LOG_INFO, "received %zu bytes from %s", packet_size, conn_host);
     
-    error = pthread_mutex_lock(connection->io_mutex);
-    int write_status = 0;
-    if (error == 0) { // if locked
-        write_status = putchars(fd, packet, packet_size);
-        if (write_status == 0) {
-            syslog(LOG_INFO, "bytes written to %s", TMPFILE);
+#ifdef USE_AESD_CHAR_DEVICE
+    if (strncmp(packet, "AESDCHAR_IOCSEEKTO:", 19) == 0) {
+
+        // We do ioctl
+        syslog(LOG_INFO, "received string %.*s", (int) packet_size-1, packet);
+        struct aesd_seekto seekto;
+
+        char *start = packet + 19, *end = NULL;
+        error = strtoul(packet+19, &end, 10);
+        if (error == ULONG_MAX) {
+            syslog(LOG_ERR, "strtoul: %s", strerror(errno));
+            abort = true;
+            goto cleanup;
+        }
+        syslog(LOG_INFO, "parsed write_cmd = %.*s", (int) (end - start), start);
+        seekto.write_cmd = error;
+
+        start = end+1; end = NULL;
+        error = strtoul(start, &end, 10);
+        if (error == ULONG_MAX) {
+            syslog(LOG_ERR, "strtoul: %s", strerror(errno));
+            abort = true;
+            goto cleanup;
+        }
+        syslog(LOG_INFO, "parsed write_cmd_offset = %.*s", (int) (end - start), start);
+        seekto.write_cmd_offset = error;
+
+        if (ioctl(fd, AESDCHAR_IOCSEEKTO, &seekto) < 0) {
+            syslog(LOG_ERR, "ioctl: %s", strerror(errno));
+            abort = true;
+            goto cleanup;
         }
 
-        error = pthread_mutex_unlock(connection->io_mutex);
-        if (error != 0) {
+    } else {
+#endif
+
+        if ((error = pthread_mutex_lock(connection->io_mutex))) {
+            syslog(LOG_ERR, "pthread_mutex_lock: %s", strerror(error));
+        }
+
+        int write_status = putchars(fd, packet, packet_size);
+
+        if ((error = pthread_mutex_unlock(connection->io_mutex))) {
             syslog(LOG_ERR, "pthread_mutex_unlock: %s", strerror(error));
         }
-    } else { 
-        syslog(LOG_ERR, "pthread_mutex_lock: %s", strerror(error));
-    }
-    
-    free(packet);
 
-    if (error != 0 || write_status < 0) {
-        abort = true;
-        goto cleanup;
-    }
+        if (error != 0 || write_status < 0) {
+            abort = true;
+            goto cleanup;
+        }
+        syslog(LOG_INFO, "bytes written to %s", TMPFILE);
+
+        // move to file start for reading
+        if (lseek(fd, 0, SEEK_SET) == (off_t) -1) {
+            syslog(LOG_ERR, "lseek: %s", strerror(errno));
+            abort = true;
+            goto cleanup;
+        }
+
+#ifdef USE_AESD_CHAR_DEVICE
+    } // end else
+#endif
 
     // Send the whole content of the file to the connected client.
     char buffer[CONN_BUFSIZE];
-
-#ifndef USE_AESD_CHAR_DEVICE
-    if (lseek(fd, 0, SEEK_SET) == (off_t) -1) {
-        syslog(LOG_ERR, "lseek: %s", strerror(errno));
-        abort = true;
-        goto cleanup;
-    }
-#endif
 
     ssize_t bytes_read;
     while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0) {
@@ -127,13 +160,18 @@ void* conn_handler(void* handler_arg) {
         abort = true;
     }
 
-cleanup: 
+  cleanup: 
+    // Free received packet
+    free(packet);
+
+  cleanup_fd:
     // Close file and connection, then exit thread
     if (close(fd) < 0) {
         syslog(LOG_ERR, "close: %s", strerror(errno));
         abort = true;
     }
 
+  finalize:
     if (close(connection->descriptor) < 0) {
         syslog(LOG_ERR, "close: %s", strerror(errno));
         abort = true;
